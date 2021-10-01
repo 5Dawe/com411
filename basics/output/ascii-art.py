fa27a455c8101b3cc10417aa91776eb10fa6705c
Steven Dawe <5dawes65@solent.ac.uk>
2021-10-01 12:13:41 +0100

    Added code to display an ascii art robot.

---
+++ basics/output/ascii-art.py
@@ -0,0 +1,13 @@
+# Display a box
+print("  ######## ")
+print("/# __  __ #\\")
+print("\\#        #/")
+print("  ########")
+print("   _----_")
+print(" /|      |\\")
+print("/ |      | \\")
+print("   -____-")
+
+
+
+
