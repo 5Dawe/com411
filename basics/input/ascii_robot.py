ff6536efeb90c63512cbb0964bbac1b867a6e5da
Steven Dawe <5dawes65@solent.ac.uk>
2021-10-01 12:26:41 +0100

    Added code to dispaly an ASCII robot with dynamic eyes.

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,12 @@
+# user input for eye of robot
+print("Please enter a character for the eye...")
+roboteye = input()
+
+print("  ######## ")
+print(f"/#  {roboteye}  {roboteye}  #\\")
+print("\\#        #/")
+print("  ########")
+print("   _----_")
+print(" /|      |\\")
+print("/ |      | \\")
+print("   -____-")
