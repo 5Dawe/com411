e701b2d46d64494ac3fb6a65b73c1545fad4dbb3
Steven Dawe <5dawes65@solent.ac.uk>
2021-10-08 11:39:21 +0100

    Added code to demonstrate the use of a modulus operator.

---
+++ basics/Simple Decision/Modulo_operator.py
@@ -0,0 +1,7 @@
+# Ask for input of a number
+print("\nPlease enter a whole number.")
+usernumber = int(input(">"))
+if usernumber % 2 != 0:
+    print(f"{usernumber} is an odd number")
+else: print(f"{usernumber} is an even number")
+
--- basics/Simple Decision/if_elif_else.py
+++ basics/Simple Decision/if_elif_else.py
@@ -1,15 +1,13 @@
-#Instructions for painting
+# Instructions for painting
 print("Towards which direction should I paint (up, down, left, right)?")
 paintdirection = str(input(">").lower())
-if paintdirection  == "up":
+if paintdirection == "up":
     print("I am painting in the upwards direction!")
-elif paintdirection  == "down":
+elif paintdirection == "down":
     print("I am painting in the downwards direction!")
-elif paintdirection  == "left":
+elif paintdirection == "left":
     print("I am painting in the left direction!")
-elif paintdirection  == "right":
+elif paintdirection == "right":
     print("I am painting in the right direction!")
 else:
     print("I don't know that direction")
-
-
