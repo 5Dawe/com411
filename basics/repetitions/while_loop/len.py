4acc302029aaf80e63fcba823004809f72d7c404
Steve Dawe <Sdawe@peta.co.uk>
2021-10-15 11:40:32 +0100

    Added code to demonstrate the use of a while loop with len function.

---
+++ basics/repetitions/while_loop/len.py
@@ -0,0 +1,15 @@
+# Beep tries to communicate
+
+# declare variable for iterations
+iterations = 0
+
+print(f"\nPlease enter a phrase:")
+phrase = str(input(">"))
+phraselen = len(phrase)
+
+while iterations < phraselen:
+    print(f"Bop ", end="")
+    iterations = iterations + 1
+
+
+# Beep removes cables
