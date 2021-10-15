627166324930b937b228870fe7c31eae9ac3a343
Steve Dawe <Sdawe@peta.co.uk>
2021-10-15 12:01:31 +0100

    Added code to demonstrate the use of summing with a while loop.

---
+++ basics/repetitions/while_loop/sum_100.py
@@ -0,0 +1,15 @@
+# Beep will calculate the sum of the first 100 numbers
+
+# declare variables
+iterations = 0
+thesum = 0
+
+print(f"\nCalculating the sum of the first 100 numbers...")
+
+#start while loop to add numbers
+
+while iterations < 100:
+    iterations = iterations + 1
+    thesum = thesum + iterations
+
+print (f"\nDone! the answer is {thesum}")
