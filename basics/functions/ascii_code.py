2c1c325c9aedf3158ba1275eae58695f38ce4443
Steve Dawe <Sdawe@peta.co.uk>
2021-10-17 14:07:42 +0100

    Added code to demonstrate the use of built-in functions: abs, range, chr.

--- basics/functions/ascii_code.py
+++ basics/functions/ascii_code.py
@@ -1,11 +1,13 @@
 # A program that determines the ASCII code for a character.
 print(f"Program started!")
-print(f"Please enter a standard character:")
-u_ch = str(input())
-if len(u_ch) == 1:
-    print(f"The ASCII code for {u_ch} is: ", end="")
-    print(ord(u_ch))
+print(f"Please enter an ASCII code:")
+u_asc = abs(int(input()))
+
+
+if u_asc in range (32, 127, 1):
+    print(f"The character represented by the ASCII code {u_asc} is: ", end="")
+    print(chr(u_asc))
 else:
-    print(f"Sorry, please enter a single character")
+    print(f"Sorry, please enter a character from the printable range")
 print(f"Program ended!")
 
