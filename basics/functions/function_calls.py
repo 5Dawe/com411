1272c5fde482b669d13020dfbedcd4141f33061b
Steve Dawe <Sdawe@peta.co.uk>
2021-10-17 16:14:25 +0100

    incomplete

---
+++ basics/functions/function_calls.py
@@ -0,0 +1,14 @@
+# A program which manipulate cryptic words.
+def run()
+    print(f"Please enter some text")
+    u_imp = str(input())
+    print(f"What should i do with the text?")
+    print(f"1) Display in a box")
+    print(f"2) Display lower case")
+    print(f"3) Display upper case")
+    print(f"4) Display in a box")
+    print(f"5) Display in a box")
+
+
+# Run the program
+run()
