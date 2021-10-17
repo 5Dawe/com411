eb9646198d2bfa9c681634269d726dbc8d9f5f3a
Steve Dawe <Sdawe@peta.co.uk>
2021-10-17 11:42:58 +0100

    Added code to demonstrate the use of a simple for loop.

--- basics/repetitions/for_loop/range.py
+++ basics/repetitions/for_loop/range.py
@@ -1,13 +1,19 @@
 # Beep adjusts night vision based on user input
 
 print(f"\nWhat level of brightness is required?")
-bri_req = int(input(">"))
+bri_req = (int(input(">"))+1)
 
 print(f"Adjusting brightness level...")
 for count in range(2, bri_req, 2):
     print(f"Beep's brightness level:", end="")
-    for beep_bri in range(0, {count}, 1):
-        print(f"X", end="")
+    for beep_bri in range(0, count, 1):
+        print(f"*", end="")
+    print(f"")
+    print(f"Bop's brightness level:", end="")
+    for bop_bri in range(0, count, 1):
+        print(f"*", end="")
+    print(f"\n")
+    print (f"{count}")
 
 
 print(f"\nAdjustments complete!")
