75a9ce306301c474cfd5c2d1192d6077c27644ef
Steve Dawe <Sdawe@peta.co.uk>
2021-11-04 22:02:09 +0000

    Added code to demonstrate a simple set.

---
+++ data/sets/simple_set.py
@@ -0,0 +1,13 @@
+# A program to help Beep and Bop record what they can see in the distance.
+
+# define a function to populate a set
+def observed():
+    observations = set()
+    observations = {"Flying Car", "Sky Scraper", "Sky Scraper", "Laser", "Dome", "Dome"}
+    return observations
+
+def run():
+    print(observed())
+
+if __name__ == "__main__":
+    run()
