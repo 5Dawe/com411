4f03ee1003912fe054bb7d055dabd43149461848
Steven Dawe <5dawes65@solent.ac.uk>
2021-11-05 11:56:51 +0000

    Added code to implement a text user interface.

---
+++ data/olympics/tui.py
@@ -0,0 +1,53 @@
+# olympics Test User Interface
+
+
+# A function named started that takes 1 parameter named msg.
+# The default value of the parameter is an empty string.
+def started(msg=""):
+    print(f"----------------------------")
+    print(f"Operation started: {msg}...\n")
+
+
+# A function named completed that takes no parameters.
+def completed():
+    print(f"Operation completed.")
+    print(f"----------------------------")
+
+# A function named error that takes 1 parameter named msg.
+# The function should display the message "Error! {msg}"
+# where {msg} is the value passed as a parameter.
+def error(msg):
+    print(f"Error! {msg}")
+
+
+# A function named menu that takes no parameters.
+# The function should display a menu
+def menu():
+    print(f"Please select one of the following options:")
+    print(f"[years]    List unique years")
+    print(f"[tally]      Tally up medals")
+    print(f"[ctally]    Tally up medals for each team")
+    print(f"[exit]       Exit the program")
+    print(f"\nYour selection:")
+    return input()
+
+
+
+# The function should display the name of each medal and
+# its count with "Gold" displayed first and "Bronze" displayed last.
+def display_medal_tally(tally):
+    print(f"{tally}")
+
+# The function should display the name of each team and the medals
+# the team has one.
+def display_medal_tally(team_tally):
+    print(f"{team_tally}")
+
+
+# The function should sort the years into descending order
+# (largest first) and display each year on a new line
+def display_years(years):
+    print(years)
+
+
+
