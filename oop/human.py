476074b0278740d37bb247ea851f5a4bbbac1fe0
Steve Dawe <Sdawe@peta.co.uk>
2021-12-10 09:45:13 +0000

    Updated classes human and robot with methods to display objects.

--- oop/human.py
+++ oop/human.py
@@ -1,17 +1,23 @@
 
-
 class Human:
-  MAX_ENERGY = 100
-  def __init__(self):
-    self.name = "Human"
-    self.age = 0
-    self.energy = Human.MAX_ENERGY
+    MAX_ENERGY = 100
+    def __init__(self):
+        self.name = "Human"
+        self.age = 0
+        self.energy = Human.MAX_ENERGY
 
-  def display(self):
-    print(f"I am {self.name}")
+    def display(self):
+        print(f"I am {self.name}")
+
+    def __repr__(self):
+        return f'Human(name={self.name}, age={self.age})'
+
+    def __str__(self):
+        return f'Human {self.name} is {self.age} years old.'
 
 
 if (__name__ == "__main__"):
-  Human = Human()
-  Human.display()
-
+    Human = Human()
+    Human.display()
+    print(Human)
+    print(repr(Human))
--- oop/robot.py
+++ oop/robot.py
@@ -1,16 +1,23 @@
 
 class Robot:
-  MAX_ENERGY = 100
-  def __init__(self):
-    self.name = "Robot"
-    self.age = 0
-    self.energy = 0
+    MAX_ENERGY = 100
+    def __init__(self):
+        self.name = "Robot"
+        self.age = 0
+        self.energy = 0
 
-  def display(self):
-    print(f"I am {self.name}")
+    def display(self):
+        print(f"I am {self.name}")
+
+    def __repr__(self):
+        return f'robot(name={self.name}, age={self.age})'
+
+    def __str__(self):
+        return f'Robot {self.name} is {self.age} years old.'
 
 
 if (__name__ == "__main__"):
-  Robot = Robot()
-  Robot.display()
-
+    Robot = Robot()
+    Robot.display()
+    print(Robot)
+    print(repr(Robot))
